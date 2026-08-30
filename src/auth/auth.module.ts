474b3acd4d8bc0b8d268da01fe257bba714e0761
Andressa Lessa <s.andressalessa@gmail.com>
2026-08-30 19:56:21 -0300

    fix: changing folder name to plural

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
-import { AuthService } from './service/auth.service';
+import { AuthService } from './services/auth.service';
 
 @Module({
   imports: [
