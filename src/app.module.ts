fafd8cffb212cea916dee702b6608093b6f5d923
Andressa Lessa <s.andressalessa@gmail.com>
2026-08-31 19:54:58 -0300

    feat: swagger changes and fixing guards folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,41 +1,55 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ThrottlerModule } from '@nestjs/throttler';
 import { ProxyModule } from './proxy/proxy.module';
 import { MiddlewareModule } from './middleware/middleware.module';
 import { LoggingMiddleware } from './middleware/logging/logging.middleware';
 import { AuthModule } from './auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { CustomThrottlerGuard } from './guards/throttler.guard';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    ThrottlerModule.forRoot([
-      {
-        name: 'short',
-        ttl: 1000, // 1 sec
-        limit: 10, // 10 requests per minute
-      },
-      {
-        name: 'medium',
-        ttl: 60000, // 1 minute
-        limit: 100, // 100 requests per minute
-      },
-      {
-        name: 'long',
-        ttl: 900000, // 15 minutes
-        limit: 1000, // 100 requests per minute
-      },
-    ]),
+    // forRootAsync is needed because another service is been used, so first he needs to load it, to then use it
+    // dependency injection
+    ThrottlerModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => [
+        {
+          name: 'short',
+          ttl: 1000, // 1 sec
+          limit: configService.get<number>('RATE_LIMIT_SHORT', 10), // 10 requests per minute
+        },
+        {
+          name: 'medium',
+          ttl: 60000, // 1 minute
+          limit: configService.get<number>('RATE_MEDIUM_SHORT', 100), // 100 requests per minute
+        },
+        {
+          name: 'long',
+          ttl: 900000, // 15 minutes
+          limit: configService.get<number>('RATE_LONG_SHORT', 1000), // 100 requests per minute
+        },
+      ],
+      inject: [ConfigService],
+    }),
     ProxyModule,
     MiddlewareModule,
     AuthModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: CustomThrottlerGuard,
+    },
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
--- src/guards/throttler.guard.ts
+++ src/guards/throttler.guard.ts
@@ -1,9 +1,46 @@
 import { Injectable } from '@nestjs/common';
-import { ThrottlerGuard } from '@nestjs/throttler';
+import {
+  ThrottlerException,
+  ThrottlerGuard,
+  ThrottlerRequest,
+} from '@nestjs/throttler';
 
 @Injectable()
 export class CustomThrottlerGuard extends ThrottlerGuard {
   protected async getTracker(req: Record<string, any>): Promise<string> {
     return `${req.ip}-${req.headers['user-agent']}`;
+  }
+
+  protected async handleRequest(reqProps: ThrottlerRequest): Promise<boolean> {
+    const { context, limit, ttl } = reqProps;
+    const { req, res } = this.getRequestResponse(context);
+    const tracker = await this.getTracker(req);
+    const trottlerName = reqProps.throttler.name ?? 'throttler-name';
+    const key = this.generateKey(context, tracker, trottlerName);
+
+    const totalHits = await this.storageService.increment(
+      key,
+      ttl,
+      limit,
+      1,
+      trottlerName,
+    );
+
+    if (Number(totalHits) > limit) {
+      res.setHeader('Retry-After', Math.round(ttl / 1000));
+      throw new ThrottlerException();
+    }
+
+    res.setHeader(`${this.headerPrefix}-Limit`, reqProps.limit);
+    res.setHeader(
+      `${this.headerPrefix}-Remaining`,
+      reqProps.limit - Number(totalHits),
+    );
+    res.setHeader(
+      `${this.headerPrefix}-Reset`,
+      Math.round(reqProps.ttl / 1000),
+    );
+
+    return true;
   }
 }
